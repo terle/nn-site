2a4109474dcfee08f750c812eda62f0a209b92a7
terle <terkel.brix@gmail.com>
2023-02-24 18:33:20 +0100

    Remove default content Add Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+
+    <v-toolbar app elevation="12">
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">
+        TEST
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn>
+          KNAAAAAPPPP
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+    
+
+
+    </v-content>
+    <v-footer>
+      
+    </v-footer>
+  </v-app>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
