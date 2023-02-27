eb70e455ae8d8b99cdba6b392544cd17dd27dcd0
terle <terkel.brix@gmail.com>
2023-02-27 15:13:35 +0100

    use plugin `vite-plugin-vuetify`

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,23 +3,18 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
 import { md2, md3 } from 'vuetify/blueprints'
 
 export default createVuetify({
-//   blueprint: md3, //using blueprints https://next.vuetifyjs.com/en/features/blueprints
-  components,
-  directives,
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-  icons: {
-    defaultSet: 'mdi',
-    aliases,
-    sets: {
-      mdi,
-    }
-  },
+    //   blueprint: md3, //using blueprints https://next.vuetifyjs.com/en/features/blueprints
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        }
+    },
 })
